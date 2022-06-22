5c9fbe9d82ed59d41466490f4dbd8467ac276b1e
ozan çakır <71380452+theozyme@users.noreply.github.com>
2022-06-22 23:26:37 +0300

    Add files via upload

---
+++ index.php
@@ -0,0 +1,41 @@
+<html>
+    <head>
+        <title>Cakır's Garage</title>
+        <meta charset="utf-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+        <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css">
+        <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.1.1/jquery.min.js"></script> 
+        <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js"></script> 
+     </head>
+<meta http-equiv="Content-Type"
+content="text/html; charset=UTF-8" />
+<body>
+     <div class="jumbotron text-center">
+      <h1>Cakır's Garage</h1>
+      <p>Otomasyon Sistemi</p>
+      <nav class="navbar navbar-inverse">
+        <div class="container-fluid">
+          <div class="navbar-header">
+            <a class="navbar-brand" href="#">Cakır's Garage</a>
+          </div>
+          <ul class="nav navbar-nav">
+            <li class="active"><a href="index.php">Ana Sayfa</a></li>
+            <li><a href="kayitformu.php">Kayıt Gir</a></li>
+            <li><a href="listele.php">Kayıtları Listele</a></li>
+            <li><a href="silmelistesi.php">Kayıt Sil</a></li>
+            <li><a href="guncellelistesi.php">Kayıt Güncelle</a></li>
+          
+          </ul>
+        </div>
+      </nav>
+      <div id="about" class="container-fluid">
+        <div class="row">
+          <div class="col-sm-8">
+            <h2>About Company Page</h2><br>
+            <h4>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.</h4><br>
+            <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.</p>
+            <br><button class="btn btn-default btn-lg">BİZE ULAŞIN</button>
+          </div>
+
+</body>
+</html
